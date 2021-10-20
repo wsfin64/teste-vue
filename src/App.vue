048db586d31f2cb3cdb52e1c16361242c7d2d5e0
Wellington da Silva <wsfin64@gmail.com>
2021-10-20 19:52:14 -0300

    inclusao de um menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
-
     <b-container id="main">
+      <div id="menu">
+        <b-navbar type="dark" variant="dark">
+          <b-navbar-nav>
+            <b-nav-item href="#">Home</b-nav-item>
+
+            <!-- Navbar dropdowns -->
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown text="User" right>
+              <b-dropdown-item href="#">Account</b-dropdown-item>
+              <b-dropdown-item href="#">Settings</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-navbar>
+      </div>    
 
       <div id="erro">{{ erro }}</div>
       <b-row>
